d01018ff5cce854e1a8bcc6fda7b94a9a05ae61a
Webcodelearn <amoskipkorir1325@gmail.com>
2024-07-13 18:25:31 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -57,7 +57,7 @@ closeBtn.addEventListener('click', () => {
 // Function to set a timeout for the next popup
 function setScrollTimeout() {
     clearTimeout(scrollTimeout);
-    scrollTimeout = setTimeout(displayPopup, 50000); // Display the popup after 3 seconds of scrolling
+    scrollTimeout = setTimeout(displayPopup, 3000); // Display the popup after 3 seconds of scrolling
 }
 
 // Event listener for scroll
